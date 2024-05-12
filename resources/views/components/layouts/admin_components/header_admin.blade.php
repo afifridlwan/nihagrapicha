56b1f860a8bc2edd2698808ea1d4cc10fa6bc202
Lukman Hadi <lukmanhadi2000@gmail.com>
2024-05-12 14:43:51 +0700

    testing

--- resources/views/components/layouts/admin_components/header_admin.blade.php
+++ resources/views/components/layouts/admin_components/header_admin.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Toko Niha Grapciha</title>
+    <title>Toko NihaGrapciha</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
